5f7318e1e63b83a9cf39cd6bb84bd87f6adcffb0
SergioGonz88 <134458871+SergioGonz88@users.noreply.github.com>
2024-05-24 00:31:38 +0000

    release version 4.0

--- movies_app.py
+++ movies_app.py
@@ -6,7 +6,7 @@ from google.oauth2 import service_account
 import json
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="names-project-demo")
+db = firestore.Client(credentials=creds, project="reto-firebase-sergio")
 dbNames = db.collection("movies")
 st.header("Nuevo registro")
 company = st.text_input("Company")
